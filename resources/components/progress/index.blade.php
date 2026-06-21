845ff405f400d294d05b9faa7547ae64c18a8610
Tony Lea <tony@devdojo.com>
2026-06-21 17:38:43 -0400

    Addin updateFixes

--- resources/components/progress/index.blade.php
+++ resources/components/progress/index.blade.php
@@ -2,16 +2,20 @@
     'value' => 0,
     'max' => 100,
     'size' => 'md',
+    'color' => null,
     'indeterminate' => false,
 ])
 
 @php
     $pct = (float) $max > 0 ? min(100, max(0, ((float) $value / (float) $max) * 100)) : 0;
+    $safeColor = $color ? preg_replace('/[^a-z0-9-]/', '', strtolower($color)) : null;
     $track = match ($size) {
+        'xs' => 'h-1.5',
         'sm' => 'h-1',
         'lg' => 'h-3',
         default => 'h-2',
     };
+    $trackBg = $safeColor ? 'bg-elevated' : 'bg-secondary';
 @endphp
 
 @once
@@ -31,9 +35,11 @@
     @unless ($indeterminate) aria-valuenow="{{ $value }}" @endunless
     {{ $attributes->twMerge('w-full') }}
 >
-    <div class="w-full overflow-hidden rounded-full bg-secondary {{ $track }}">
+    <div class="w-full overflow-hidden rounded-full {{ $trackBg }} {{ $track }}">
         @if ($indeterminate)
             <div class="dd-progress-bar-indeterminate h-full w-1/3 rounded-full bg-primary"></div>
+        @elseif ($safeColor)
+            <div class="h-full rounded-full transition-[width] duration-300 ease-out" style="width: {{ $pct }}%; background-color: var(--dot-{{ $safeColor }})"></div>
         @else
             <div class="h-full rounded-full bg-primary transition-[width] duration-300 ease-out" style="width: {{ $pct }}%"></div>
         @endif
